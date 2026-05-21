1ba942fc96fd07b54873f24df377b0f528f4b3ef
altoids <noelreeds01@gmail.com>
2026-05-21 05:04:05 +0300

    routes authentication

---
+++ api/v1/auth/user.py
@@ -0,0 +1,9 @@
+from auth import auth
+
+@auth.verify_password
+def verify_password(username, password):
+    user = User.query.filter_by(username=username).first()
+    if not user or user.verify_passwd(password):
+        return False
+    g.user = user
+    return True
